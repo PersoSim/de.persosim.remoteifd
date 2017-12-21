d894607569b2bea279a41b932199c4ca0cfb6421
Alexander May <alexander.may@secunet.com>
2017-12-21 15:42:50 +0100

    Initial UI for RemoteIFD

---
+++ de.persosim.remoteifd.ui/src/de/persosim/remoteifd/ui/handlers/ConfigureRemoteIfdHandler.java
@@ -0,0 +1,16 @@
+ 
+package de.persosim.remoteifd.ui.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.globaltester.logging.BasicLogger;
+import org.globaltester.logging.tags.LogLevel;
+
+public class ConfigureRemoteIfdHandler {
+	@Execute
+	public void execute() {
+		//FIXME implement this
+		BasicLogger.log(this.getClass(), "Configure Remote IFD", LogLevel.FATAL);
+		
+	}
+		
+}
---
+++ de.persosim.remoteifd.ui/src/de/persosim/remoteifd/ui/handlers/RemoteIfdHandler.java
@@ -0,0 +1,15 @@
+ 
+package de.persosim.remoteifd.ui.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.globaltester.logging.BasicLogger;
+import org.globaltester.logging.tags.LogLevel;
+
+public class RemoteIfdHandler {
+	@Execute
+	public void execute() {
+		//FIXME implement this
+		BasicLogger.log(this.getClass(), "Switch to use RemoteIfd", LogLevel.FATAL);
+	}
+		
+}
