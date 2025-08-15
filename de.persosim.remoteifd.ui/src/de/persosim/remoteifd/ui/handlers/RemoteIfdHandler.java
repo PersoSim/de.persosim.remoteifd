2f26699cb4b329a937d05152a8150aaf2a5cca80
Stefan Philipp <stefan.philipp@secunet.com>
2025-08-15 12:21:26 +0200

    PS-27: Fix problem "Interface change via menu only works with interface tab in foreground"

--- de.persosim.remoteifd.ui/src/de/persosim/remoteifd/ui/handlers/RemoteIfdHandler.java
+++ de.persosim.remoteifd.ui/src/de/persosim/remoteifd/ui/handlers/RemoteIfdHandler.java
@@ -1,38 +1,50 @@
- 
+
 package de.persosim.remoteifd.ui.handlers;
 
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.menu.MItem;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.globaltester.logging.BasicLogger;
 import org.globaltester.logging.tags.LogLevel;
 
-import de.persosim.driver.connector.service.IfdConnector;
 import de.persosim.driver.connector.ui.parts.ReaderPart;
 import de.persosim.websocket.WebsocketComm;
+import jakarta.inject.Inject;
 
-public class RemoteIfdHandler {
-	IfdConnector connector;
-	
+public class RemoteIfdHandler
+{
+	private EPartService partService;
+
+	@Inject
+	public RemoteIfdHandler(EPartService partService)
+	{
+		this.partService = partService;
+	}
+
 	@CanExecute
-	public boolean checkState(final MPart mPart, final MItem mItem) {
-		if (mPart.getObject() instanceof ReaderPart) {
-			ReaderPart readerPartObject = (ReaderPart) mPart.getObject();
-			mItem.setSelected(readerPartObject.getCurrentCommType() == WebsocketComm.NAME);
+	public boolean checkState(final MItem mItem)
+	{
+		// ID of part as defined in fragment.e4xmi application model
+		MPart readerPart = partService.findPart("de.persosim.driver.connector.ui.parts.reader");
+		if (readerPart.getObject() instanceof ReaderPart mPart) {
+			ReaderPart readerPartObject = mPart;
+			mItem.setSelected(WebsocketComm.NAME.equals(readerPartObject.getCurrentCommType()));
 		}
 		return true;
 	}
-	
+
 	@Execute
-	public void execute(final MPart mPart) {
+	public void execute()
+	{
 		BasicLogger.log(this.getClass(), "RemoteIfd menu entry toggled", LogLevel.INFO);
-		
-		if (mPart.getObject() instanceof ReaderPart) {
-			ReaderPart readerPartObject = (ReaderPart) mPart.getObject();
-			
+		// ID of part as defined in fragment.e4xmi application model
+		MPart readerPart = partService.findPart("de.persosim.driver.connector.ui.parts.reader");
+		if (readerPart.getObject() instanceof ReaderPart mPart) {
+			ReaderPart readerPartObject = mPart;
 			readerPartObject.switchReaderType(new WebsocketComm(null, de.persosim.remoteifd.ui.Activator.getRemoteIfdConfig()));
 		}
 	}
-		
+
 }
